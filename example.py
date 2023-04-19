073de9af280420d39c9538eb8f09207512bcd21c
Kokyou <707376149@qq.com>
2023-04-19 10:06:08 +0200

    Minor

--- example.py
+++ example.py
@@ -11,7 +11,7 @@ ori_model = tf.keras.models.load_model("models/cifar10/resnet20.h5")
 drop_model = tf.keras.models.load_model("models/cifar10/resnet20_drop.h5")
 x_test -= x_train_mean
 base_map = drop_check_func(ori_model, drop_model, x_test[:5000], y_test[:5000], 50)
-base_acc = 0.8728
+base_acc = 0.8728 # accuracy of labeled test data
 estimated_acc, acc1, acc2 = Aries_estimation(x_test[5000:], drop_model=drop_model, base_map=base_map, base_acc=base_acc, section_num=50)
 print("Acc: {}, {}, {}".format(estimated_acc, acc1, acc2))
 
